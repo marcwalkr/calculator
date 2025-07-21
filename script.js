40ec2b2d73b9114c17897550f2a190f85167dbb9
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-07-21 13:15:37 -0500

    Implement number, operator, and equals logic
    - Handle updating the display for number inputs and result evaluations
    - Handle operator inputs, including chaining, e.g. 1 + 2 - 3
    - Handle equals, including multiple in a row to repeat operation

--- script.js
+++ script.js
@@ -32,12 +32,85 @@ function appendToDisplay(value) {
   display.scrollLeft = display.scrollWidth;
 }
 
+function clearDisplay() {
+  display.textContent = "";
+}
+
+function handleNumber(value) {
+  if (operation === null) {
+    num1 += value;
+  } else if (num2 === "") {
+    clearDisplay();
+    num2 += value;
+  } else {
+    num2 += value;
+  }
+
+  appendToDisplay(value);
+}
+
+function handleOperator(value) {
+  if (num1 === "") return;
+
+  if (justEvaluated) {
+    operation = value;
+    num2 = "";
+    justEvaluated = false;
+    return;
+  }
+
+  if (num2 === "") {
+    operation = value;
+  } else {
+    num1 = operate(Number(num1), Number(num2), operation);
+    clearDisplay();
+    appendToDisplay(num1);
+
+    num2 = "";
+    operation = value;
+  }
+}
+
+
+function handleEquals() {
+  if (num1 !== "" && num2 !== "" && operation !== null) {
+    num1 = operate(Number(num1), Number(num2), operation);
+    clearDisplay();
+    appendToDisplay(num1);
+
+    justEvaluated = true;
+  }
+}
+
 const display = document.querySelector("#display-text");
+
+let num1 = "";
+let num2 = "";
+let operation = null;
+let justEvaluated = false;
 
 const buttons = document.querySelector("#button-container");
 buttons.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
   const value = event.target.dataset.value;
-  if (!value) return // Click wasn't on a number button
 
-  appendToDisplay(value);
+  if (!type) return // Click wasn't on a button
+
+  switch (type) {
+    case "number":
+      handleNumber(value);
+      break;
+    case "decimal":
+      break;
+    case "operator":
+      handleOperator(value);
+      break;
+    case "equals":
+      handleEquals();
+      break;
+    case "clear":
+      break;
+    case "delete":
+      break;
+  }
 });
